deb01386d8277ffe6f05fde19d3d3e773b6e9f97
Shaoli Lu <slu@rwbaird.com>
2023-01-17 21:31:16 -0600

    Edge

--- SeleniumWeb/SeleniumTest.cs
+++ SeleniumWeb/SeleniumTest.cs
@@ -17,20 +17,24 @@ namespace SeleniumWeb
         {
             By googleSearchBar = By.Name("q");
             By googleSearchButton = By.Name("btnK");
-            By googleResultText = By.XPath(".//h2//span[text()='Street Fighter V']");
+            //By googleResultText = By.XPath(".//h2//span[text()='Street Fighter V']");
+            By googleResultText = By.ClassName("ssJ7i"); // XPath(".//rhs//div[text()='Street Fighter V']");
 
-            //IWebDriver webDriver = new EdgeDriver();
-            ChromeDriverManager.InstallLatest();
+            IWebDriver webDriver = new EdgeDriver();
+            //ChromeDriverManager.InstallLatest();
             //IWebDriver webDriver = new ChromeDriver();
             //IWebDriver webDriver = new FirefoxDriver();
             webDriver.Manage().Window.Maximize();
 
-            webDriver.Navigate().GoToUrl("https://www.google.com/"); 
+            webDriver.Navigate().GoToUrl("https://www.google.com/");
+            IWebElement googleSearchBox = webDriver.FindElement(By.Name("q"));
             webDriver.FindElement(googleSearchBar).SendKeys("Street Fighter V");
             //webDriver.FindElement(googleSearchButton).Click();
             //webDriver.Navigate().Forward();
+            googleSearchBox.Submit();
 
             var actualResultText = webDriver.FindElement(googleResultText);
+            var testText = actualResultText.Text;
             Assert.IsTrue(actualResultText.Text.Equals("Street Fighter V"));
 
             webDriver.Quit();
